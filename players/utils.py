6c324a4f4dd713d70dcda040abb309b78d3649c6
taqpol <njosephchen@gmail.com>
2018-07-25 16:12:04 -0700

    code reorganization
    removed relevant code from webhooks folder, deleted it, and re-filed it under players

--- players/utils.py
+++ players/utils.py
@@ -1,6 +1,28 @@
 import requests
 from discord_bot.settings import API_KEY
 
+
+headers = {'Authorization': API_KEY, 'Accept':'application/vnd.api+json'}
+
 def get_player_elo(player_info):
 	return player_info.json()['data'][0]['attributes']['stats']['rankPoints']
+
+
+def retrieve_player_info(player_name):
+	if ''.join(list(filter(lambda x: x.isalpha() or x == '_', list(player_name)))) != player_name:
+		response = requests.Response()
+		response.status_code = 400
+		response.reason = 'Bad Request'
+		return response
+	else:
+		return requests.get('https://api.dc01.gamelockerapp.com/shards/na/players/', 
+		headers=headers, params={'filter[playerNames]':'{}'.format(player_name)})
+
+
+def retrieve_match_telemetry(match_id):
+	pass
+
+
+def retrieve_match_data(match_id):
+	pass
 
